109aaf095c148336c080389ef9dfa791e3d38a60
Lavinia Karl <laviniakarl@Bugaboo.local>
2014-07-07 16:08:26 -0700

    Innitial commit

---
+++ model.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+
+
+# ENGINE = create_engine("INSERT THING HERE PLZ", echo = False)
+# Session = scoped_session(sessionmaker(bind = ENGINE, autocommit = False, autoflush = False))
+
+# Base = declarative_base()
+# Base.query = Session.query_property()
+
+####Classes got here (Do I need classes?  What are they? Line, System, et?)
---
+++ routes.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, redirect, request
+import model
+
+app = Flask(__name__)
+app.secret_key = '\xdd$j\x8dX\x19\xe69\x08"t/\'K\x1c\x1di"\'C\x8d*(\xd2'
+
+####Routes and stuff go here (@app.route())####
+@app.route('/', methods=['GET'])
+def load_options():
+	return render_template('options-page.html')
+
+
+@app.route('/', methods=['POST'])
+def load_map():
+	routes=request.form['route']
+	lines=request.form['line']
+	time=request.form['time']
+	print time
+	print routes
+	print lines
+	return redirect('/map')
+
+@app.route('/map')
+def show_map():
+	return render_template('map.html')
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
