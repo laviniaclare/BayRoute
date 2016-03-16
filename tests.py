6a2138b5bd4441c39dd12a231b110876ba8b206b
laviniaclare <laviniaclare@gmail.com>
2016-03-15 17:13:49 -0700

    stubbed out some test classes

--- tests.py
+++ tests.py
@@ -5,5 +5,18 @@ import routes
 
 ####### Tests go here #######
 
+
+class IntegrationTest(unittest.TestCase):
+
+    def test_load_options(self):
+        pass
+
+    def test_prepare_routes_for_display(self):
+        pass
+
+
+class ModelTest(unittest.TestCase):
+    pass
+
 if __name__ == '__main__':
     unittest.main()
