9ac007a799a4526ad5da964af97e15e0422705c3
laviniaclare <laviniaclare@gmail.com>
2016-05-16 11:39:49 -0700

    writing test for get_routes_by_id. Havining difficulty importing function

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from model import Agency, Route, Trip, StopTime, Stop
+from server import get_routes_by_id
 
 import unittest
 import routes
@@ -15,8 +16,10 @@ class IntegrationTest(unittest.TestCase):
     def test_prepare_routes_for_display(self):
         pass
 
-    def test_get_routes_by_id(self):
-        pass
+    def test_get_routes_by_id_one_route(self):
+        result = get_routes_by_id("['CT_SHUTTLE']")
+        expected_result = [{u'5660669': [[37.3103897344, -121.8827510483], [37.3291912336, -121.9019291905]], u'5660682': [[37.3291912336, -121.9019291905], [37.3103897344, -121.8827510483]]}]
+        self.assertEqual(result, expected_result)
 
 
 class ModelTest(unittest.TestCase):
